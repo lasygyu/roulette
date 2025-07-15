b5527c405382c77f6f405fe8aab03268caed2f79
-3σ <yurippe@m3sigma.net>
2025-07-15 17:41:54 +0900

    -

--- index.js
+++ index.js
@@ -32,6 +32,7 @@ async function getJujakInfos() {
   console.log(jujakInfo);
   if (!jujakInfo) {
     const new_jj = {
+      sw: false,
       targetName: "",
       param: 1,
       revParam: 1,
@@ -48,16 +49,16 @@ async function getJujakInfos() {
 
 function getReady() {
   getJujakInfos().then((jujakInfos) => {
-    const { targetName, param, revParam, weightParam, revWeightParam, nerfParam } = jujakInfos;
+    const { sw, targetName, param, revParam, weightParam, revWeightParam, nerfParam } = jujakInfos;
     const names = getNames();
     window.roullete.setMarbles(
       names,
-      targetName,
-      param,
-      revParam,
-      weightParam,
-      revWeightParam,
-      nerfParam
+      sw ? targetName : "",
+      sw ? param: 1,
+      sw ? revParam: 1,
+      sw ? weightParam: 1,
+      sw ? revWeightParam: 1,
+      sw ? nerfParam : 1
     );
     ready = names.length > 0;
     localStorage.setItem('mbr_names', names.join(','));
